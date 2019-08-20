d7e9601c09a006543bfb766ea2f864fc8dbbba52
Antonio Djigo <antonio@secret-source.eu>
2019-08-20 16:24:38 +0100

    Rooms migrations

---
+++ database/migrations/2019_08_20_142145_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('room_name')->charset('utf8');
+            $table->integer('counter_value')->default(0);
+            $table->integer('max_capacity')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
